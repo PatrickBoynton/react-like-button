71bde764eb06b60be3dbcc262d2b01f79e9b73a4
PatrickBoynton <jpboynton3@gmail.com>
2021-02-08 14:20:14 -0500

    Added a like state.

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ class App extends React.Component {
 
     this.state = {
       number: 0,
+      likes: "Likes", 
     }
     
     this.handleIncrement = this.handleIncrement.bind(this);
   }
   handleIncrement() {
-    this.setState({number: this.state.number += 1});
+    this.setState({number: this.state.number + 1, likes: (this.state.number >= 1) && (this.state.number !== 0) ? "Likes" : "Like"});
   }
+
   render() {
     return (
       <React.Fragment>
-        <button onClick={() => this.handleIncrement()}>{this.state.number}</button>
+        <button onClick={() => this.handleIncrement()}>{this.state.likes} {this.state.number}</button>
       </React.Fragment>
     )
   }
