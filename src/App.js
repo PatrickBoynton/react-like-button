9909c2d2df5bd77f5c16b40cdc9136a41b8dfa09
PatrickBoynton <jpboynton3@gmail.com>
2021-02-08 13:01:57 -0500

    Added a new constructor.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    }
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
   render() {
     return (
       <React.Fragment>
